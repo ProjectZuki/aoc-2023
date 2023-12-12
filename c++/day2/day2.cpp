a9a3796efb8585f70f235bb103f0dadef1cda6d9
Willie Alcaraz <63901327+ProjectZuki@users.noreply.github.com>
2023-12-11 22:52:33 -0800

    Day 2 Solution
    Day 2 part 1 solution.
    ... will worry about optimization later....

--- c++/day2/day2.cpp
+++ c++/day2/day2.cpp
@@ -7,93 +7,79 @@
 #include <sstream>
 
 int main () {
-    std::stack<std::vector<int>> games;
 
     std::ifstream inFile;
-    inFile.open("sample.txt");
-    std::string gamestr;
-    int gamenum;
-    char garb;
-    int red_cube    = 0;
-    int green_cube  = 0;
-    int blue_cube   = 0;
-
-    std::string cube;
-    int count;
-    std::string line;
-    int idsum = 0;
+    inFile.open("input.txt");
 
     if (inFile.is_open() == false) {
         std::cout << "Error opening file" << std::endl;
         return 1;
     }
 
-    bool ispossible = false;
+    int idsum = 0;
+    std::string line;
+    bool ispossible = true;
+    bool endgroup = false;
         
     while (std::getline(inFile, line)) {
         // std::cout << "\nSTART LINE" << std::endl;
         std::istringstream iss(line);
         std::string gameTag;
-        bool endgroup = false;
+
+        int gamenum;
+        char garb;
+        int red_cube    = 0;
+        int green_cube  = 0;
+        int blue_cube   = 0;
+        int count;
+        std::string cube;
 
         iss >> gameTag >> gamenum >> garb;
         while (iss >> count >> cube) {
+
+            // Process cube (red, green, blue)
             if (cube.back() == ',') {
                 cube = cube.substr(0, cube.length() - 1);
-            } else if (cube.back() == ';') {
-                cube = cube.substr(0, cube.length() - 1);
+            } else {
+                if (cube.back() == ';') {
+                    cube = cube.substr(0, cube.length() - 1);
+                }
                 endgroup = true;
             }
-            // std::cout << "cube = " << cube << " count = " << count << std::endl;
-            // Process cube (red, green, blue)
-            if (cube == "red") {
-                red_cube += count;
-            } else if (cube == "green") {
-                green_cube += count;
-            } else if (cube == "blue") {
-                blue_cube += count;
-            }
 
+            if (cube == "red") {
+                red_cube = count;
+            } else if (cube == "green") {
+                green_cube = count;
+            } else if (cube == "blue") {
+                blue_cube = count;
+            }
+            
             if (endgroup) {
-                
-                // std::cout << "\nNEXT GROUP" << std::endl;
-                if (red_cube <= 12 && green_cube <= 13 && blue_cube <= 14) {
-                    idsum += gamenum;
-                    ispossible = true;
-                    std::cout << std::endl;
+                if (red_cube > 12 || green_cube > 13 || blue_cube > 14) {
+                    ispossible = false;
                 }
+                red_cube = 0;
+                green_cube = 0;
+                blue_cube = 0;
                 endgroup = false;
             }
-
-            // std::cout << "NEXT GROUP" << std::endl;
-        //     std::cout << "red: " << red_cube << std::endl;
-        //     std::cout << "green: " << green_cube << std::endl;
-        //     std::cout << "blue: " << blue_cube << std::endl << std::endl;
         }
-        // total_red += red_cube;
-        // total_green += green_cube;
-        // total_blue += blue_cube;
-        // std::cout << "Game #" << gamenum << std::endl;
-        // std::cout << "Total red = " << red_cube << std::endl;
-        // std::cout << "Total green = " << green_cube << std::endl;
-        // std::cout << "Total blue = " << blue_cube << std::endl;
 
-        // if (red_cube <= 12 && green_cube <= 13 && blue_cube <= 14) {
-        //     std::cout << "Game #" << gamenum << " Possible\n";
-        //     idsum += gamenum;
-        //     std::cout << std::endl;
-        // } else {
-        // }
+        if (ispossible) {
+            idsum += gamenum;
+        }
 
-        red_cube = 0;
-        green_cube = 0;
-        blue_cube = 0;
+        // reset values
+        ispossible = true;
     }
 
-    std::cout << "idsum = " << idsum << std::endl;
+    std::cout << "\n sum of id's = " << idsum << std::endl;
 
     inFile.close();
 
 
     return 0;
 }
+
+// ans: 2176
